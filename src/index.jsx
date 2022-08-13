a56a82e20c7ada62f01ba622d778508fe5ce32f3
napster <napster@pop-os.localdomain>
2022-08-13 19:34:48 +0530

    pwa

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import { Provider } from "react-redux"
+import store from "./redux/store"
+import * as seviceWorkerRegistration from "./serviceWorkerRegistration"
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
-);
+)
+
+seviceWorkerRegistration.register()
