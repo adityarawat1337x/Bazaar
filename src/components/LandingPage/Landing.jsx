ea980a83af0c6f09208838a03cb99abb1bd99fe0
Aditya <napster@pop-os.localdomain>
2022-08-14 17:25:02 +0530

    done

--- src/components/LandingPage/Landing.jsx
+++ src/components/LandingPage/Landing.jsx
@@ -1,10 +1,33 @@
-import React from "react";
-import Model from "./model";
-
-import "./Landing.css";
+import React from "react"
+import Model from "./model"
+import styled from "styled-components"
+import "./Landing.css"
 
 function Landing() {
-  return <Model />;
+  return (
+    <>
+      <Model />;
+      <Cont>
+        <h1>Explore the Heritage</h1>
+      </Cont>
+    </>
+  )
 }
 
-export default Landing;
+const Cont = styled.div`
+  margin-top: -20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #fafafa;
+  color: black;
+  font-size: 2rem;
+  transition: 0.6;
+  @media only screen and (max-width: 600px) {
+    font-size: 1em;
+  }
+`
+
+export default Landing
